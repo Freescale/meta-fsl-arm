likewise/meta-freescale

linux-imx-2.6.35.3: Added IPU patches fixing  ERESTARTSYS behaviour.
Posted by Phil Endecott on imxcommunity.org.

Signed-off-by: Leon Woestenberg <leon@sidebranch.com>

recipes-kernel/linux/linux-imx_2.6.35.3.bb
@@ -4,7 +4,7 @@
 DESCRIPTION = "Linux kernel for imx platforms"
 LICENSE = "GPLv2"
 LIC_FILES_CHKSUM = "file://COPYING;md5=d7810fab7487fb0aad327b76f1be7cd7"
-PR = "r9"
+PR = "r10"
 
 inherit kernel
 COMPATIBLE_MACHINE = "(imx53qsb|imx53ard)"
@@ -26,6 +26,9 @@ SRC_URI = "git://opensource.freescale.com/pub/scm/imx/linux-2.6-imx.git;tag=rel_
            file://1146-ENGR00170244-1-ARM-AHCI-Enable-PDDQ-mode-when-no-d.patch \
            file://1147-ENGR00170244-2-ARM-AHCI-Enable-PDDQ-mode-when-no-d.patch \
            file://no-unaligned-access.patch \
+           file://001_Add_accept4_syscall.patch \
+           file://002_Return_ERESTARTSYS_from_IPU_GET_EVENT.patch \
+           file://003_Sanitise_ipu_interrupt_return_value.patch \
            file://defconfig \
           "
 

Freescale/meta-fsl-arm

Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

recipes-kernel/linux/linux-imx_2.6.35.3.bb
@@ -4,7 +4,7 @@
 DESCRIPTION = "Linux kernel for imx platforms"
 LICENSE = "GPLv2"
 LIC_FILES_CHKSUM = "file://COPYING;md5=d7810fab7487fb0aad327b76f1be7cd7"
-PR = "r11"
+PR = "r12"
 
 inherit kernel
 COMPATIBLE_MACHINE = "(imx53qsb|imx53ard|imx51evk)"
@@ -27,6 +27,9 @@ SRC_URI = "git://opensource.freescale.com/pub/scm/imx/linux-2.6-imx.git;tag=rel_
            file://1147-ENGR00170244-2-ARM-AHCI-Enable-PDDQ-mode-when-no-d.patch \
            file://no-unaligned-access.patch \
            file://0001-mx53_loco-add-USR-led-support.patch \
+           file://001_Add_accept4_syscall.patch \
+           file://002_Return_ERESTARTSYS_from_IPU_GET_EVENT.patch \
+           file://003_Sanitise_ipu_interrupt_return_value.patch \
            file://defconfig \
           "
 