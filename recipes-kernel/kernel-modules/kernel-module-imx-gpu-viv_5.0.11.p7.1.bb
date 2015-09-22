Freescale/meta-fsl-arm

commit d476c351dc5ec18d2d58de6537b897c52c43a779

kernel-module-imx-gpu-viv: Upgrade to 5.0.11.p7.1 for 3.14.38-6QP_ga release
- Package name is changed to kernel-module-imx-gpu-viv.
- Patch platform-Fix-kernel-version-check-for-3.14-based-ker.patch is no longer required
  as the fix has been applied upstream.
- Upgrade to graphics version 5.0.11.p7.1
- Makefile patch will be included in next release.

Signed-off-by: Neena Busireddy <neena.busireddy@freescale.com>
Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

recipes-kernel/kernel-modules/kernel-module-imx-gpu-viv/platform-Fix-kernel-version-check-for-3.14-based-ker.patch
@@ -1,35 +1,0 @@
-From 617bdbec386a1237e2a148989318cc4a1360788a Mon Sep 17 00:00:00 2001
-From: Otavio Salvador <otavio@ossystems.com.br>
-Date: Tue, 18 Aug 2015 23:08:48 +0000
-Subject: [PATCH] platform: Fix kernel version check for 3.14-based kernels
-Organization: O.S. Systems Software LTDA.
-
-The build fail about the bus frequency header (linux/busfreq-imx6.h)
-not being found is caused by the mistaken check for the wrong kernel
-version.
-
-This patch fixes it by adding the right kernel version to be checked.
-
-Upstream-Status: Pending
-
-Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>
----
- .../os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx6q14.c | 2 +-
- 1 file changed, 1 insertion(+), 1 deletion(-)
-
-diff --git a/imx-gpu-viv-kernel-src/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx6q14.c b/imx-gpu-viv-kernel-src/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx6q14.c
-index a2e72ff..241614a 100644
---- a/imx-gpu-viv-kernel-src/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx6q14.c
-+++ b/imx-gpu-viv-kernel-src/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx6q14.c
-@@ -40,7 +40,7 @@
- #include <linux/pm_runtime.h>
- #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 10, 0)
- #include <mach/busfreq.h>
--#elif LINUX_VERSION_CODE < KERNEL_VERSION(3, 14, 0)
-+#elif LINUX_VERSION_CODE < KERNEL_VERSION(3, 15, 0)
- #include <linux/busfreq-imx6.h>
- #include <linux/reset.h>
- #else
--- 
-2.1.4
-

recipes-kernel/kernel-modules/kernel-module-imx-gpu-viv_5.0.11.p7.1.bb
@@ -1,0 +1,16 @@
+# Copyright (C) 2015 Freescale Semiconductor
+
+SUMMARY = "Kernel loadable module for Vivante GPU"
+DESCRIPTION = "Provides flexibility to switch graphics between different kernels in future \
+releases. This package uses same source code as GPU kernel driver source."
+LICENSE = "GPLv2"
+LIC_FILES_CHKSUM = "file://COPYING;md5=12f884d2ae1ff87c09e5b7ccc2c4ca7e"
+
+inherit module
+
+SRC_URI = "${FSL_MIRROR}/${PN}-${PV}.tar.gz \
+           file://updatemakefile.patch \
+           "
+
+SRC_URI[md5sum] = "a251a94390986371f75b338ad938e46f"
+SRC_URI[sha256sum] = "9aaef0a62bc2be69dc568228192b060c54970b5c700fee602d83a4d13e04a9b3"

recipes-kernel/kernel-modules/kernel-module-imx-gpu-viv/updatemakefile.patch
@@ -1,0 +1,158 @@
+Upstream-Status: Pending
+
+Signed-off-by: Neena Busireddy <neena.busireddy@freescale.com>
+
+diff -Naur kernel-module-imx-gpu-viv-5.0.11.p7.1_org/kernel-module-imx-gpu-viv-src/Makefile kernel-module-imx-gpu-viv-5.0.11.p7.1/kernel-module-imx-gpu-viv-src/Makefile
+--- kernel-module-imx-gpu-viv-5.0.11.p7.1_org/kernel-module-imx-gpu-viv-src/Makefile	2015-09-22 10:46:18.280385659 -0500
++++ kernel-module-imx-gpu-viv-5.0.11.p7.1/kernel-module-imx-gpu-viv-src/Makefile	2015-09-22 10:46:51.000000000 -0500
+@@ -1,20 +1,54 @@
+ ##############################################################################
+ #
+-#    Copyright (C) 2005 - 2014 by Vivante Corp.
++#    The MIT License (MIT)
+ #
+-#    This program is free software; you can redistribute it and/or modify
+-#    it under the terms of the GNU General Public License as published by
+-#    the Free Software Foundation; either version 2 of the license, or
+-#    (at your option) any later version.
++#    Copyright (c) 2014 Vivante Corporation
++#
++#    Permission is hereby granted, free of charge, to any person obtaining a
++#    copy of this software and associated documentation files (the "Software"),
++#    to deal in the Software without restriction, including without limitation
++#    the rights to use, copy, modify, merge, publish, distribute, sublicense,
++#    and/or sell copies of the Software, and to permit persons to whom the
++#    Software is furnished to do so, subject to the following conditions:
++#
++#    The above copyright notice and this permission notice shall be included in
++#    all copies or substantial portions of the Software.
++#
++#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
++#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
++#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
++#    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
++#    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
++#    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
++#    DEALINGS IN THE SOFTWARE.
++#
++##############################################################################
++#
++#    The GPL License (GPL)
++#
++#    Copyright (C) 2014  Vivante Corporation
++#
++#    This program is free software; you can redistribute it and/or
++#    modify it under the terms of the GNU General Public License
++#    as published by the Free Software Foundation; either version 2
++#    of the License, or (at your option) any later version.
+ #
+ #    This program is distributed in the hope that it will be useful,
+ #    but WITHOUT ANY WARRANTY; without even the implied warranty of
+-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
++#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ #    GNU General Public License for more details.
+ #
+ #    You should have received a copy of the GNU General Public License
+-#    along with this program; if not write to the Free Software
+-#    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
++#    along with this program; if not, write to the Free Software Foundation,
++#    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
++#
++##############################################################################
++#
++#    Note: This software is released under dual MIT and GPL licenses. A
++#    recipient may use this file under the terms of either the MIT license or
++#    GPL License. If you wish to use only one license not the other, you can
++#    indicate your decision by deleting one of the above license notices in your
++#    version of this file.
+ #
+ ##############################################################################
+ 
+@@ -23,16 +57,15 @@
+ # Linux build file for kernel HAL driver.
+ #
+ 
+-AQROOT ?= .
+-
+ include $(AQROOT)/config
+ 
+-KERNEL_DIR ?= $(KERNEL_PATH)
++KERNEL_DIR ?= $(TOOL_DIR)/kernel
+ 
+ OS_KERNEL_DIR   := hal/os/linux/kernel
+ ARCH_KERNEL_DIR := hal/kernel/arch
+ ARCH_VG_KERNEL_DIR := hal/kernel/archvg
+ HAL_KERNEL_DIR  := hal/kernel
++HOST := $(shell hostname)
+ 
+ # Check and include platform config.
+ ifneq ($(PLATFORM),)
+@@ -63,6 +96,14 @@
+         $(OS_KERNEL_DIR)/gc_hal_kernel_debugfs.o \
+         $(OS_KERNEL_DIR)/gc_hal_kernel_allocator.o \
+ 
++ifneq ($(CONFIG_DMA_SHARED_BUFFER),)
++OBJS += $(OS_KERNEL_DIR)/allocator/default/gc_hal_kernel_allocator_dmabuf.o
++endif
++
++ifneq ($(CONFIG_IOMMU_SUPPORT),)
++OBJS += $(OS_KERNEL_DIR)/gc_hal_kernel_iommu.o
++endif
++
+ ifneq ($(PLATFORM),)
+ OBJS += $(OS_KERNEL_DIR)/gc_hal_kernel_probe.o
+ OBJS += $(OS_KERNEL_DIR)/platform/$(PLATFORM).o
+@@ -83,6 +124,10 @@
+ OBJS += $(ARCH_KERNEL_DIR)/gc_hal_kernel_context.o \
+         $(ARCH_KERNEL_DIR)/gc_hal_kernel_hardware.o
+ 
++ifeq ($(VIVANTE_ENABLE_3D), 1)
++OBJS += $(ARCH_KERNEL_DIR)/gc_hal_kernel_recorder.o
++endif
++
+ ifeq ($(VIVANTE_ENABLE_VG), 1)
+ OBJS +=\
+           $(HAL_KERNEL_DIR)/gc_hal_kernel_vg.o\
+@@ -99,10 +144,6 @@
+ OBJS += $(OS_KERNEL_DIR)/gc_hal_kernel_sync.o
+ endif
+ 
+-ifneq ($(CONFIG_ANDROID),)
+-EXTRA_CFLAGS += -DANDROID
+-endif
+-
+ ifeq ($(SECURITY), 1)
+ OBJS += $(OS_KERNEL_DIR)/gc_hal_kernel_security_channel.o \
+         $(HAL_KERNEL_DIR)/gc_hal_kernel_security.o
+@@ -118,15 +159,16 @@
+ 
+ # Define targets.
+ all:
+-	@make V=$(V) ARCH=$(ARCH_TYPE) -C $(KERNEL_DIR) SUBDIRS=`pwd` modules
++	@$(MAKE) V=$(V) ARCH=$(ARCH_TYPE) -C $(KERNEL_DIR) SUBDIRS=`pwd` modules
+ 
+ clean:
+ 	@rm -rf $(OBJS)
+-	@rm -rf modules.order Module.symvers
++	@rm -rf modules.order Module.symvers .tmp_versions
+ 	@find $(AQROOT) -name ".gc_*.cmd" | xargs rm -f
+ 
+ install: all
+ 	@mkdir -p $(SDK_DIR)/drivers
++	@cp $(MODULE_NAME).ko $(SDK_DIR)/drivers
+ 
+ else
+ 
+@@ -261,8 +303,10 @@
+ EXTRA_CFLAGS += -I$(AQROOT)/hal/kernel/archvg
+ endif
+ 
+-obj-m += galcore.o
++EXTRA_CFLAGS += -DHOST=\"$(HOST)\"
++
++obj-m = $(MODULE_NAME).o
+ 
+-galcore-objs  := $(OBJS)
++$(MODULE_NAME)-objs  = $(OBJS)
+ 
+ endif

recipes-kernel/kernel-modules/kernel-module-imx-gpu-viv_5.0.11.p6.3.bb
@@ -1,17 +1,0 @@
-# Copyright (C) 2015 Freescale Semiconductor
-
-SUMMARY = "Kernel loadable module for Vivante GPU"
-DESCRIPTION = "Provides flexibility to switch graphics between different kernels in future \
-releases. This package uses same source code as GPU kernel driver source."
-LICENSE = "GPLv2"
-LIC_FILES_CHKSUM = "file://COPYING;md5=12f884d2ae1ff87c09e5b7ccc2c4ca7e"
-
-inherit module
-
-SRC_URI = "${FSL_MIRROR}/imx-gpu-viv-kernel-${@'${PV}'.replace('5.0.11.p6.3', '5.0.11.p6.3-beta')}.tar.gz \
-           file://platform-Fix-kernel-version-check-for-3.14-based-ker.patch"
-
-SRC_URI[md5sum] = "6d46da80de94e98ee68ab1a75f384b89"
-SRC_URI[sha256sum] = "e4b02fc0c9bdbfc7ecc67a0bad0917e788921c8f2444d99bd77daae7f3cd95df"
-
-S = "${WORKDIR}/imx-gpu-viv-kernel-${@'${PV}'.replace('5.0.11.p6.3', '5.0.11.p6.3-beta')}"

commit 4e44a7efab9259b6e4536b549b374f5550769d4c

kernel-module-imx-gpu-viv: Fix build against 3.14-based kernels - again
The build fail about the bus frequency header (linux/busfreq-imx6.h)
not being found is caused by the mistaken check for the wrong kernel
version.

The regression has been introduced by d476c35
(kernel-module-imx-gpu-viv: Upgrade to 5.0.11.p7.1 for 3.14.38-6QP_ga
release) which has been not properly tested. This patch fixes it,
again, by adding back the removed patch.

Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

recipes-kernel/kernel-modules/kernel-module-imx-gpu-viv/platform-Fix-kernel-version-check-for-3.14-based-ker.patch
@@ -1,0 +1,35 @@
+From 617bdbec386a1237e2a148989318cc4a1360788a Mon Sep 17 00:00:00 2001
+From: Otavio Salvador <otavio@ossystems.com.br>
+Date: Tue, 18 Aug 2015 23:08:48 +0000
+Subject: [PATCH] platform: Fix kernel version check for 3.14-based kernels
+Organization: O.S. Systems Software LTDA.
+
+The build fail about the bus frequency header (linux/busfreq-imx6.h)
+not being found is caused by the mistaken check for the wrong kernel
+version.
+
+This patch fixes it by adding the right kernel version to be checked.
+
+Upstream-Status: Pending
+
+Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>
+---
+ .../os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx6q14.c | 2 +-
+ 1 file changed, 1 insertion(+), 1 deletion(-)
+
+diff --git a/kernel-module-imx-gpu-viv-src/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx6q14.c b/kernel-module-imx-gpu-viv-src/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx6q14.c
+index a2e72ff..241614a 100644
+--- a/kernel-module-imx-gpu-viv-src/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx6q14.c
++++ b/kernel-module-imx-gpu-viv-src/hal/os/linux/kernel/platform/freescale/gc_hal_kernel_platform_imx6q14.c
+@@ -40,7 +40,7 @@
+ #include <linux/pm_runtime.h>
+ #if LINUX_VERSION_CODE < KERNEL_VERSION(3, 10, 0)
+ #include <mach/busfreq.h>
+-#elif LINUX_VERSION_CODE < KERNEL_VERSION(3, 14, 0)
++#elif LINUX_VERSION_CODE < KERNEL_VERSION(3, 15, 0)
+ #include <linux/busfreq-imx6.h>
+ #include <linux/reset.h>
+ #else
+-- 
+2.1.4
+

recipes-kernel/kernel-modules/kernel-module-imx-gpu-viv_5.0.11.p7.1.bb
@@ -10,7 +10,7 @@ inherit module
 
 SRC_URI = "${FSL_MIRROR}/${PN}-${PV}.tar.gz \
            file://updatemakefile.patch \
-           "
+           file://platform-Fix-kernel-version-check-for-3.14-based-ker.patch"
 
 SRC_URI[md5sum] = "a251a94390986371f75b338ad938e46f"
 SRC_URI[sha256sum] = "9aaef0a62bc2be69dc568228192b060c54970b5c700fee602d83a4d13e04a9b3"